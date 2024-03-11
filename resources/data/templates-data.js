055944e8d9c9b649e4b56864716043f643060d38
Zlatin Zhelyazkov <zlatinzhelyazkov98@gmail.com>
2024-03-11 08:23:40 +0200

    Fixing url paths

--- resources/data/templates-data.js
+++ resources/data/templates-data.js
@@ -1,407 +1,407 @@
 var templates = [
     {
-        image: "/resources/images/templates/Layout-01.webp",
+        image: "./resources/images/templates/Layout-01.webp",
         title: "Layout #01",
         themeColor: "#92c447"
     },
 
     {
-        image: "/resources/images/templates/Layout-02.webp",
+        image: "./resources/images/templates/Layout-02.webp",
         title: "Layout #02",
         themeColor: "#7cc432"
     },
 
     {
-        image: "/resources/images/templates/Layout-03.webp",
+        image: "./resources/images/templates/Layout-03.webp",
         title: "Layout #03",
         themeColor: "#00b3b7"
     },
 
     {
-        image: "/resources/images/templates/Layout-04.webp",
+        image: "./resources/images/templates/Layout-04.webp",
         title: "Layout #04",
         themeColor: "#ff514f"
     },
 
     {
-        image: "/resources/images/templates/Layout-05.webp",
+        image: "./resources/images/templates/Layout-05.webp",
         title: "Layout #05",
         themeColor: "#e6ce50"
     },
 
     {
-        image: "/resources/images/templates/Layout-06.webp",
+        image: "./resources/images/templates/Layout-06.webp",
         title: "Layout #06",
         themeColor: "#75b529"
     },
 
     {
-        image: "/resources/images/templates/Layout-07.webp",
+        image: "./resources/images/templates/Layout-07.webp",
         title: "Layout #07",
         themeColor: "#1664a2"
     },
 
     {
-        image: "/resources/images/templates/Layout-08.webp",
+        image: "./resources/images/templates/Layout-08.webp",
         title: "Layout #08",
         themeColor: "#8dbe03"
     },
 
     {
-        image: "/resources/images/templates/Layout-09.webp",
+        image: "./resources/images/templates/Layout-09.webp",
         title: "Layout #09",
         themeColor: "#294023"
     },
 
     {
-        image: "/resources/images/templates/Layout-10.webp",
+        image: "./resources/images/templates/Layout-10.webp",
         title: "Layout #10",
         themeColor: "#0b2138"
     },
 
     {
-        image: "/resources/images/templates/Layout-11.webp",
+        image: "./resources/images/templates/Layout-11.webp",
         title: "Layout #11",
         themeColor: "#e6b33e"
     },
 
     {
-        image: "/resources/images/templates/Layout-12.webp",
+        image: "./resources/images/templates/Layout-12.webp",
         title: "Layout #12",
         themeColor: "#216196"
     },
 
     {
-        image: "/resources/images/templates/Layout-13.webp",
+        image: "./resources/images/templates/Layout-13.webp",
         title: "Layout #13",
         themeColor: "#404145"
     },
 
     {
-        image: "/resources/images/templates/Layout-14.webp",
+        image: "./resources/images/templates/Layout-14.webp",
         title: "Layout #14",
         themeColor: "#cbee30"
     },
 
     {
-        image: "/resources/images/templates/Layout-15.webp",
+        image: "./resources/images/templates/Layout-15.webp",
         title: "Layout #15",
         themeColor: "#8fbb23"
     },
 
     {
-        image: "/resources/images/templates/Layout-16.webp",
+        image: "./resources/images/templates/Layout-16.webp",
         title: "Layout #16",
         themeColor: "#358475"
     },
 
     {
-        image: "/resources/images/templates/Layout-17.webp",
+        image: "./resources/images/templates/Layout-17.webp",
         title: "Layout #17",
         themeColor: "#de0c31"
     },
 
     {
-        image: "/resources/images/templates/Layout-18.webp",
+        image: "./resources/images/templates/Layout-18.webp",
         title: "Layout #18",
         themeColor: "#d4ae97"
     },
 
     {
-        image: "/resources/images/templates/Layout-19.webp",
+        image: "./resources/images/templates/Layout-19.webp",
         title: "Layout #19",
         themeColor: "#67b960"
     },
 
     {
-        image: "/resources/images/templates/Layout-20.webp",
+        image: "./resources/images/templates/Layout-20.webp",
         title: "Layout #20",
         themeColor: "#1b6296"
     },
 
     {
-        image: "/resources/images/templates/Layout-21.webp",
+        image: "./resources/images/templates/Layout-21.webp",
         title: "Layout #21",
         themeColor: "#d25134"
     },
 
     {
-        image: "/resources/images/templates/Layout-22.webp",
+        image: "./resources/images/templates/Layout-22.webp",
         title: "Layout #22",
         themeColor: "#030303"
     },
 
     {
-        image: "/resources/images/templates/Layout-23.webp",
+        image: "./resources/images/templates/Layout-23.webp",
         title: "Layout #23",
         themeColor: "#f0f0f0"
     },
 
     {
-        image: "/resources/images/templates/Layout-24.webp",
+        image: "./resources/images/templates/Layout-24.webp",
         title: "Layout #24",
         themeColor: "#7e909a"
     },
 
     {
-        image: "/resources/images/templates/Layout-25.webp",
+        image: "./resources/images/templates/Layout-25.webp",
         title: "Layout #25",
         themeColor: "#fbb3c9"
     },
 
     {
-        image: "/resources/images/templates/Layout-26.webp",
+        image: "./resources/images/templates/Layout-26.webp",
         title: "Layout #26",
         themeColor: "#07bcf3"
     },
 
     {
-        image: "/resources/images/templates/Layout-27.webp",
+        image: "./resources/images/templates/Layout-27.webp",
         title: "Layout #27",
         themeColor: "#06261b"
     },
 
     {
-        image: "/resources/images/templates/Layout-28.webp",
+        image: "./resources/images/templates/Layout-28.webp",
         title: "Layout #28",
         themeColor: "#b4e531"
     },
 
     {
-        image: "/resources/images/templates/Layout-29.webp",
+        image: "./resources/images/templates/Layout-29.webp",
         title: "Layout #29",
         themeColor: "#eb928a"
     },
 
     {
-        image: "/resources/images/templates/Layout-30.webp",
+        image: "./resources/images/templates/Layout-30.webp",
         title: "Layout #30",
         themeColor: "#fdf0ea"
     },
 
     {
-        image: "/resources/images/templates/Layout-31.webp",
+        image: "./resources/images/templates/Layout-31.webp",
         title: "Layout #31",
         themeColor: "#09a381"
     },
 
     {
-        image: "/resources/images/templates/Layout-32.webp",
+        image: "./resources/images/templates/Layout-32.webp",
         title: "Layout #32",
         themeColor: "#f4c949"
     },
 
     {
-        image: "/resources/images/templates/Layout-33.webp",
+        image: "./resources/images/templates/Layout-33.webp",
         title: "Layout #33",
         themeColor: "#bd535d"
     },
 
     {
-        image: "/resources/images/templates/Layout-34.webp",
+        image: "./resources/images/templates/Layout-34.webp",
         title: "Layout #34",
         themeColor: "#fa9d40"
     },
 
     {
-        image: "/resources/images/templates/Layout-35.webp",
+        image: "./resources/images/templates/Layout-35.webp",
         title: "Layout #35",
         themeColor: "#343247"
     },
 
     {
-        image: "/resources/images/templates/Layout-36.webp",
+        image: "./resources/images/templates/Layout-36.webp",
         title: "Layout #36",
         themeColor: "#96af92"
     },
 
     {
-        image: "/resources/images/templates/Layout-37.webp",
+        image: "./resources/images/templates/Layout-37.webp",
         title: "Layout #37",
         themeColor: "#323464"
     },
 
     {
-        image: "/resources/images/templates/Layout-38.webp",
+        image: "./resources/images/templates/Layout-38.webp",
         title: "Layout #38",
         themeColor: "#254f79"
     },
 
     {
-        image: "/resources/images/templates/Layout-39.webp",
+        image: "./resources/images/templates/Layout-39.webp",
         title: "Layout #39",
         themeColor: "#adb7f5"
     },
 
     {
-        image: "/resources/images/templates/Layout-40.webp",
+        image: "./resources/images/templates/Layout-40.webp",
         title: "Layout #40",
         themeColor: "#722267"
     },
     {
-        image: "/resources/images/templates/Layout-41.webp",
+        image: "./resources/images/templates/Layout-41.webp",
         title: "Layout #41",
         themeColor: "#f4c343"
     },
 
     {
-        image: "/resources/images/templates/Layout-42.webp",
+        image: "./resources/images/templates/Layout-42.webp",
         title: "Layout #42",
         themeColor: "#e00f0b"
     },
 
     {
-        image: "/resources/images/templates/Layout-43.webp",
+        image: "./resources/images/templates/Layout-43.webp",
         title: "Layout #43",
         themeColor: "#f9aa29"
     },
 
     {
-        image: "/resources/images/templates/Layout-44.webp",
+        image: "./resources/images/templates/Layout-44.webp",
         title: "Layout #44",
         themeColor: "#67bf68"
     },
 
     {
-        image: "/resources/images/templates/Layout-45.webp",
+        image: "./resources/images/templates/Layout-45.webp",
         title: "Layout #45",
         themeColor: "#3b204d"
     },
 
     {
-        image: "/resources/images/templates/Layout-46.webp",
+        image: "./resources/images/templates/Layout-46.webp",
         title: "Layout #46",
         themeColor: "#454859"
     },
 
     {
-        image: "/resources/images/templates/Layout-47.webp",
+        image: "./resources/images/templates/Layout-47.webp",
         title: "Layout #47",
         themeColor: "#193854"
     },
 
     {
-        image: "/resources/images/templates/Layout-48.webp",
+        image: "./resources/images/templates/Layout-48.webp",
         title: "Layout #48",
         themeColor: "#9a4b7a"
     },
 
     {
-        image: "/resources/images/templates/Layout-49.webp",
+        image: "./resources/images/templates/Layout-49.webp",
         title: "Layout #49",
         themeColor: "#1cc2be"
     },
 
     {
-        image: "/resources/images/templates/Layout-50.webp",
+        image: "./resources/images/templates/Layout-50.webp",
         title: "Layout #50",
         themeColor: "#ffb4a0"
     },
 
     {
-        image: "/resources/images/templates/Layout-51.webp",
+        image: "./resources/images/templates/Layout-51.webp",
         title: "Layout #51",
         themeColor: "#699325"
     },
 
     {
-        image: "/resources/images/templates/Layout-52.webp",
+        image: "./resources/images/templates/Layout-52.webp",
         title: "Layout #52",
         themeColor: "#49a9a8"
     },
 
     {
-        image: "/resources/images/templates/Layout-53.webp",
+        image: "./resources/images/templates/Layout-53.webp",
         title: "Layout #53",
         themeColor: "#ff0b41"
     },
 
     {
-        image: "/resources/images/templates/Layout-54.webp",
+        image: "./resources/images/templates/Layout-54.webp",
         title: "Layout #54",
         themeColor: "#26c188"
     },
 
     {
-        image: "/resources/images/templates/Layout-55.webp",
+        image: "./resources/images/templates/Layout-55.webp",
         title: "Layout #55",
         themeColor: "#0f8fbc"
     },
 
     {
-        image: "/resources/images/templates/Layout-56.webp",
+        image: "./resources/images/templates/Layout-56.webp",
         title: "Layout #56",
         themeColor: "#afa582"
     },
 
     {
-        image: "/resources/images/templates/Layout-57.webp",
+        image: "./resources/images/templates/Layout-57.webp",
         title: "Layout #57",
         themeColor: "#2993cf"
     },
 
     {
-        image: "/resources/images/templates/Layout-58.webp",
+        image: "./resources/images/templates/Layout-58.webp",
         title: "Layout #58",
         themeColor: "#e81d4c"
     },
 
     {
-        image: "/resources/images/templates/Layout-59.webp",
+        image: "./resources/images/templates/Layout-59.webp",
         title: "Layout #59",
         themeColor: "#e6533f"
     },
 
     {
-        image: "/resources/images/templates/Layout-60.webp",
+        image: "./resources/images/templates/Layout-60.webp",
         title: "Layout #60",
         themeColor: "#72939c"
     },
 
     {
-        image: "/resources/images/templates/Layout-61.webp",
+        image: "./resources/images/templates/Layout-61.webp",
         title: "Layout #61",
         themeColor: "#186be3"
     },
 
     {
-        image: "/resources/images/templates/Layout-62.webp",
+        image: "./resources/images/templates/Layout-62.webp",
         title: "Layout #62",
         themeColor: "#004293"
     },
 
     {
-        image: "/resources/images/templates/Layout-63.webp",
+        image: "./resources/images/templates/Layout-63.webp",
         title: "Layout #63",
         themeColor: "#60ddcf"
     },
 
     {
-        image: "/resources/images/templates/Layout-64.webp",
+        image: "./resources/images/templates/Layout-64.webp",
         title: "Layout #64",
         themeColor: "#f75270"
     },
 
     {
-        image: "/resources/images/templates/Layout-65.webp",
+        image: "./resources/images/templates/Layout-65.webp",
         title: "Layout #65",
         themeColor: "#f57876"
     },
 
     {
-        image: "/resources/images/templates/Layout-66.webp",
+        image: "./resources/images/templates/Layout-66.webp",
         title: "Layout #66",
         themeColor: "#236097"
     },
 
     {
-        image: "/resources/images/templates/Layout-67.webp",
+        image: "./resources/images/templates/Layout-67.webp",
         title: "Layout #67",
         themeColor: "#215e95"
     },
 
     {
-        image: "/resources/images/templates/Layout-68.webp",
+        image: "./resources/images/templates/Layout-68.webp",
         title: "Layout #68",
         themeColor: "#000106"
     }
